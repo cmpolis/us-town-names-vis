7603b59ae100e8a78930a8ee66430491b1fbbd6d
Chris Polis <cmpolis@gmail.com>
2016-02-20 13:27:54 -0800

    city csv, working on rendering

--- app.js
+++ app.js
@@ -4,117 +4,70 @@
 // Render US states + place names based on regex
 
 //
-var render = function(teamSeasons) {
-  var canvas = document.querySelector('#seasons-vis');
-  var c = canvas.getContext("2d");
+var render = function(err, statesGeo, cities) {
+  if(err) { return console.error('failed to load data'); }
+
+  var lastTimestamp = performance.now();
+
+  // cities = cities.map(function(d) {
+  //   return {
+  //     lat: +d.lat,
+  //     lng: +d.lng,
+  //     st: d.state,
+  //     n: d.name };
+  // });
+
+  // console.log((performance.now() - lastTimestamp) + 'ms for data manipulation');
+  // lastTimestamp = performance.now();
+
+  // // test regex query perf
+  // var sanOrSantaRE = /(San|Santa) /;
+  // var sanOrSanta = cities.filter(function(d) { return sanOrSantaRE.test(d.name); });
+  // console.log((performance.now() - lastTimestamp) + 'ms for query');
+  // lastTimestamp = performance.now();
+  // console.log(sanOrSanta);
+
+  var canvas = document.querySelector('#seasons-vis'),
+      c = canvas.getContext("2d");
   canvasDpiScaler(canvas, c);
+  var svg = d3.select(document.querySelector('svg'));
+  var width = 1000,
+      height = 600;
 
-  //
-  var histogram = {};
-  d3.range(-40, 40).forEach(function(ndx) { histogram[ndx] = 0; });
-  teamSeasons
-    .filter(function(s) { return s.games === 82; })
-    .forEach(function(s) { histogram[s.wins - 41]++; });
-  histogram = Object.keys(histogram).map(function(k) {
-    return { gamesAbove: +k, count: histogram[k] };
+  var projection = d3.geo.albersUsa()
+    .scale(width)
+    .translate([width / 2, height / 2]);
+  var path = d3.geo.path()
+    .projection(projection);
+
+  cities.forEach(function(d) {
+    d.lat = +d.lat;
+    d.lng = +d.lng;
+    d.coords = projection([d.lng, d.lat]);
   });
 
-  //
-  var width = 800,
-      height = 600,
-      yCenter = height / 2,
-      gameWidth = width / 82,
-      gameHeight = height / 160,
-      gamesAbove, gameNdx, isGSW, isHighWins;
+  console.log(cities[0])
 
-  //
-  c.clearRect(0, 0, width, height);
+  // path(topojson.feature(us, us.objects.counties));
+  //     context.stroke();
+  console.log(statesGeo.features);
+  var states = statesGeo.features;
+  svg.selectAll('path.state')
+    .data(states).enter()
+    .append('path')
+    .attr('class', 'state')
+    .attr('d', function(d) { return path(d); })
 
-  c.strokeStyle = '#999';
-  c.globalAlpha = 0.5;
-  // 'perfect season line'
-  // c.moveTo(10, yCenter - gameHeight);
-  // c.lineWidth = 1.5;
-  // c.lineTo(10 + 82 * gameWidth, yCenter - 83 * gameHeight);
-  // c.stroke();
-
-  // '.500' line
-  c.moveTo(0, yCenter);
-  c.lineTo(82 * gameWidth, yCenter);
-  c.stroke();
-  c.lineWidth = 5.5;
-
-  teamSeasons.forEach(function(season) {
-    isGSW = season.year === 2016 &&
-            season.team === 'Golden State Warriors';
-    isHighWins = season.wins >= 65;
-    gamesAbove = 0;
-    c.moveTo(0, yCenter);
-    c.beginPath();
-    for(gameNdx = 0; gameNdx < season.games; gameNdx++) {
-      c.lineTo(0 + gameNdx * gameWidth, yCenter - (gameHeight * gamesAbove));
-      season.timeline[gameNdx] ? gamesAbove++ : gamesAbove--;
-    }
-    c.lineTo(0 + gameNdx * gameWidth, yCenter - (gameHeight * gamesAbove));
-    c.strokeStyle = isGSW ? '#F05F46' : (isHighWins ? '#E9AF6F' : 'steelblue');
-    c.globalAlpha = isGSW ? 0.6 : (isHighWins ? 0.3 : 0.019);
-    c.stroke();
-  });
-
-  // svg elements
-  var svg = d3.select(document.querySelector('#season-vis-svg'));
-  var x = d3.scale.linear()
-    .domain([0,82]).range([0,width]);
-  var histogramX = d3.scale.linear()
-    .domain(d3.extent(histogram, function(d) { return d.count; })).range([0,50]);
-  var xAxis = d3.svg.axis().scale(x).orient('bottom');
-  var y = d3.scale.linear()
-    .domain([-40,40]).range([height, 0]);
-  var yAxis = d3.svg.axis().scale(y).orient('right');
-  svg.selectAll('rect').data(histogram)
-    .enter().append('rect')
-      .attr('x',width)
-      .attr('y', function(d) { return y(d.gamesAbove+0.5); })
-      .attr('width', function(d) { return histogramX(d.count); })
-      .attr('height', y(1) - y(2))
-      // .attr('opacity', function(d) { return d.count * 0.015; })
-      // .attr('opacity', function(d) { return 0.2 + d.count * 0.007 })
-      .attr('opacity', 0.4)
-      .attr('fill', 'steelblue');
-  svg.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,'+yCenter+')')
-    .call(xAxis);
-  svg.append('text')
-    .text('Games Played')
-    .attr('x', x(80))
-    .attr('y', y(0) - 4)
-    .attr('text-anchor', 'end');
-  svg.append('text')
-    .text('Games Above .500')
-    .attr('transform', 'translate('+(width-4)+','+(y(0)+30)+') rotate(-90)')
-    .attr('text-anchor', 'end');
-  svg.append('g')
-    .attr('class', 'y axis')
-    .attr('transform', 'translate('+width+',0)')
-    .call(yAxis);
-
+   svg.append('circle')
+    .attr('cx', cities[0].coords[0])
+    .attr('cy', cities[0].coords[1])
+    .attr('r', 3)
+    .attr('fill', 'red');
 };
 
-d3.csv('./us-towns.csv')
-  // .row(function(d) {
-  //   var games  = +d.games,
-  //       wins   = +d.wins,
-  //       losses = d.games - d.wins,
-  //       ratio  = d.wins / d.games,
-  //       bArray = new BitArray(games, d.wlBinary);
-  //   return {
-  //     year: +d.year,
-  //     team: d.team,
-  //     games: games,
-  //     wins: wins,
-  //     losses: losses,
-  //     ratio: ratio,
-  //     timeline: bArray.toArray().slice(0, games) };
-  // }).get(function(err, rows) { err ? console.log(err) : render(rows); });
-  .get(function(err, rows) { err ? console.log(err) : render(rows); });
+d3_queue.queue()
+  .defer(d3.json, './us_states.json')
+  .defer(d3.csv, './us-cities-lat-lng.csv')
+  .await(render);
+
+//.get(function(err, rows) { err ? console.log(err) : render(rows); });
